3ef293adabe58168e78c19b94e49214f83b4f80f
Mäki-Dator\Mäki <Marcus.Jakobsson@live.se>
2017-01-19 22:23:42 +0100

    La till rad operation och värde. Skalar dock inte perfekt

--- GUIProjekt/FontSizeConverter.cs
+++ GUIProjekt/FontSizeConverter.cs
@@ -12,7 +12,7 @@ namespace GUIProjekt
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             double actualHeight = System.Convert.ToDouble(value);
-            int fontSize = (int)(actualHeight * .024);
+            int fontSize = (int)(actualHeight * .074);
             return fontSize;
         }
         
