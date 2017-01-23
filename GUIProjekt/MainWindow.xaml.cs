3e4e2a4df017f7bbe3b18b655e7317d3c7627e58
Voroz <jonny_fherm@hotmail.com>
2017-01-23 22:05:07 +0100

    Visar Output, WorkingReg vid programstart

--- GUIProjekt/MainWindow.xaml.cs
+++ GUIProjekt/MainWindow.xaml.cs
@@ -41,6 +41,10 @@ namespace GUIProjekt
 
             // Mark current row
             markRow(getMMRowOfPosition(255 - _assemblerModel.instructionPtr()));
+
+            // TODO: update instructionPtr
+            ValueRow_WorkingRegister.ShowValue(Convert.ToString((byte)_assemblerModel.workingRegister(), 2).PadLeft(8, '0'));
+            ValueRow_Output.ShowValue(Convert.ToString((byte)_assemblerModel.output(), 2).PadLeft(8, '0'));
         }
 
 
