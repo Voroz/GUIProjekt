6beb6cce6da5c29321493e126d8ea9ba13410cfe
Voroz <jonny_fherm@hotmail.com>
2017-01-27 16:49:04 +0100

    Saknad klass

---
+++ GUIProjekt/Bit12.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIProjekt
+{
+    class Bit12
+    {
+        public Bit12(short val) {
+            _bit12Val = convertTo12Bit(val);
+        }
+
+        public static Bit12 operator +(Bit12 a, Bit12 b)
+        {
+            Bit12 newBit12 = new Bit12((short)(a.value() + b.value()));
+            return newBit12;
+        }
+
+        public static Bit12 operator -(Bit12 a, Bit12 b)
+        {
+            Bit12 newBit12 = new Bit12((short)(a.value() - b.value()));
+            return newBit12;
+        }
+
+        public static bool operator ==(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() == rhs.value();
+        }
+
+        public static bool operator !=(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() != rhs.value();
+        }
+
+        public static bool operator <(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() < rhs.value();
+        }
+
+        public static bool operator >(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() > rhs.value();
+        }
+
+        public static bool operator <=(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() <= rhs.value();
+        }
+
+        public static bool operator >=(Bit12 lhs, Bit12 rhs)
+        {
+            return lhs.value() <= rhs.value();
+        }
+
+        public short value()        {
+            return _bit12Val;
+        }
+
+        private short convertTo12Bit(short val)
+        {
+
+            while (val > 2047)
+            {
+                val -= 4096;
+            }
+            while (val < -2048)
+            {
+                val += 4096;
+            }
+
+            return val;
+        }
+
+        private short _bit12Val;
+    }
+}
