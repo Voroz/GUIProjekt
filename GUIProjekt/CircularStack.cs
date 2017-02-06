70f0e571fdbc058028495e6b9543fff49a96c8d8
GurreGuldmuskel <beefosaur@hotmail.com>
2017-02-06 16:04:05 +0100

    Avslutande punkt i errormeddelande

--- GUIProjekt/CircularStack.cs
+++ GUIProjekt/CircularStack.cs
@@ -11,7 +11,8 @@ namespace GUIProjekt {
          CALL: CircularStack<T> myStack;
          TASK: Uses an array of a certain datatype.
          *****************************************************/
-        public CircularStack(uint capacity) {
+        public CircularStack(uint capacity) 
+        {
             _usedSize = 0;
             _capacity = capacity;
             _front = 0;
@@ -22,7 +23,8 @@ namespace GUIProjekt {
          CALL: push(T val);
          TASK: Pushes the parameter onto the stack.
          *****************************************************/
-        public void push(T val) {
+        public void push(T val) 
+        {
             if (_usedSize != _capacity) {
                 _usedSize++;
             }
@@ -34,7 +36,8 @@ namespace GUIProjekt {
          CALL: pop();
          TASK: The latest element added gets deleted.
          *****************************************************/
-        public void pop() {
+        public void pop() 
+        {
             Debug.Assert(_usedSize != 0);
             if (_front == 0) {
                 _front = _capacity;
@@ -47,7 +50,8 @@ namespace GUIProjekt {
          CALL: T topVal = top();
          TASK: Returns the value on top of the stack.
          *****************************************************/
-        public T top() {
+        public T top() 
+        {
             Debug.Assert(_usedSize != 0);
             uint toReturn = _front;
             if (toReturn == 0) {
@@ -61,7 +65,8 @@ namespace GUIProjekt {
          CALL: int size = size();
          TASK: Returns the number of elements on the stack.
          *****************************************************/
-        public uint size() {
+        public uint size() 
+        {
             return _usedSize;
         }
 
--- GUIProjekt/SkinManager.cs
+++ GUIProjekt/SkinManager.cs
@@ -20,7 +20,8 @@ namespace GUIProjekt
          CALL: When choosing a skin.
          TASK: Loads the XAML file for the wanted skin.
          *****************************************************/
-        public static ResourceDictionary GetSkin(Skins skin) {
+        public static ResourceDictionary GetSkin(Skins skin) 
+        {
             return Application.LoadComponent(new Uri(getPath(skin), UriKind.Relative)) as ResourceDictionary;
         }
 
@@ -28,7 +29,8 @@ namespace GUIProjekt
          CALL: string path = getPath(Skins);
          TASK: Returns the path to the parameter skin.
          *****************************************************/
-        private static string getPath(Skins skin) {
+        private static string getPath(Skins skin) 
+        {
             switch (skin) {
                 default:
                 case Skins.Default:
